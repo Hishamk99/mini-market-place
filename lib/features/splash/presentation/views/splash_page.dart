18ba047597fb520ff1bce15889f47609003a4439
hishamkame <hishamkamel370@gmail.com>
2024-05-31 12:55:57 +0300

    create gradient for scaffold

--- lib/features/splash/presentation/views/splash_page.dart
+++ lib/features/splash/presentation/views/splash_page.dart
@@ -6,6 +6,46 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xff5C609B),
+                Color(0xff767CB2),
+                Color(0xffB6BDD0),
+                //  Color(0xffAEB4CA),
+              ],
+            ),
+          ),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Welcome to',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'NFT Marketplace',
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
