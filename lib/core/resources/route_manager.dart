ac51d30390a8665d782b064c513d24142cd7786b
hishamkame <hishamkamel370@gmail.com>
2024-06-01 01:11:31 +0300

    create home page and its route

--- lib/core/resources/route_manager.dart
+++ lib/core/resources/route_manager.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:mini_nft_marketplace_app/features/home/presentation/views/home_page.dart';
 import 'package:mini_nft_marketplace_app/features/splash/presentation/views/splash_page.dart';
 
 class RouteManager {
   static Map<String, WidgetBuilder> routes = {
      SplashPage.id : (context)=> const SplashPage(),
+     HomePage.id : (context)=> const HomePage(),
   };
 }
