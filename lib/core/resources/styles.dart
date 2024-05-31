b9a70ab2128ef3c70311493ef1b2fc0c507c0c94
hishamkame <hishamkamel370@gmail.com>
2024-05-31 20:59:57 +0300

    adding BackdropFilter to container to make blur

--- lib/core/resources/styles.dart
+++ lib/core/resources/styles.dart
@@ -2,24 +2,22 @@ import 'package:flutter/material.dart';
 
 class Styles {
   static TextStyle style36 = const TextStyle(
-      color: Colors.white,
-      fontSize: 36,
-      fontWeight: FontWeight.w900,
-      fontFamily: 'SF Pro Display',);
+    color: Colors.white,
+    fontSize: 36,
+    fontWeight: FontWeight.w900,
+    fontFamily: 'SF Pro Display',
+  );
 
-    static TextStyle style20 = const TextStyle(
-        color : Colors.white,
-        fontSize : 20,
-        fontWeight: FontWeight.w700,
-        fontFamily: 'SF Pro Display',
-    );  
-    static TextStyle style12 = const TextStyle(
-        color : Color(0xffEBEBF5),
-        fontSize : 12,
-        fontWeight: FontWeight.w400,
-        fontFamily: 'SF Pro Display',
-    );  
-    
-    
-
+  static TextStyle style20 = const TextStyle(
+    color: Colors.white,
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'SF Pro Display',
+  );
+  static TextStyle style12 = TextStyle(
+    color: const Color(0xffEBEBF5).withOpacity(.6),
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'SF Pro Display',
+  );
 }
--- lib/features/splash/presentation/views/widgets/custom_card.dart
+++ lib/features/splash/presentation/views/widgets/custom_card.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:mini_nft_marketplace_app/core/resources/strings_manager.dart';
 import 'package:mini_nft_marketplace_app/core/resources/styles.dart';
@@ -11,26 +12,32 @@ class CustomCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(27),
-      child: Container(
-        color: Colors.red,
-        width: double.infinity,
-        height: 200,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 50 , vertical: 20),
-          child: Column(
-            children: [
-              Text(
-                StringsManager.kTextSplashCard,
-                style: Styles.style20,
-              ),
-              const SizedBox(height : 5),
-              Text(
-                StringsManager.kTextSplashSubtitleCard,
-                textAlign: TextAlign.center,
-                style: Styles.style12,
-              ),
-              const Spacer(),
-            ],
+      child: BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaX: 5,
+          sigmaY: 5,
+        ),
+        child: Container(
+          color: Colors.white.withOpacity(.1),
+          width: double.infinity,
+          height: 200,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+            child: Column(
+              children: [
+                Text(
+                  StringsManager.kTextSplashCard,
+                  style: Styles.style20,
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  StringsManager.kTextSplashSubtitleCard,
+                  textAlign: TextAlign.center,
+                  style: Styles.style12,
+                ),
+                const Spacer(),
+              ],
+            ),
           ),
         ),
       ),
