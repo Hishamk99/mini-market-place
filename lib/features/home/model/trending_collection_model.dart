e24fb592500e5c6b9e092c8d12fe0a67e8603de9
hishamkame <hishamkamel370@gmail.com>
2024-06-03 00:04:14 +0300

    finish TrendingCollection model

--- lib/features/home/model/trending_collection_model.dart
+++ lib/features/home/model/trending_collection_model.dart
@@ -1,8 +1,12 @@
 class TrendingCollectionModel {
   final String image;
   final String title;
+  final String count;
+  final bool active;
 
   TrendingCollectionModel({
+    required this.count,
+    required this.active,
     required this.image,
     required this.title,
   });
