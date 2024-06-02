7b2dfacfef42d6e8626bf4b410b531c1aca0116c
hishamkame <hishamkamel370@gmail.com>
2024-06-02 14:14:29 +0300

    create appbar instead of custom tilte widget

--- lib/features/home/presentation/views/home_page.dart
+++ lib/features/home/presentation/views/home_page.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:mini_nft_marketplace_app/constant.dart';
-import 'package:mini_nft_marketplace_app/features/home/presentation/widgets/custom_home_title.dart';
+import 'package:mini_nft_marketplace_app/core/resources/strings_manager.dart';
+import 'package:mini_nft_marketplace_app/core/resources/styles.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
   static String id = 'HomePage';
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: SafeArea(
         child: Scaffold(
           backgroundColor: kPrimaryColor,
-          body: Padding(
+          appBar: AppBar(
+            title: Text(
+              StringsManager.kTitleHomePage,
+              style: Styles.style25,
+            ),
+            centerTitle: true,
+            backgroundColor: kPrimaryColor,
+          ),
+          body: const Padding(
             padding: EdgeInsets.symmetric(vertical: 12.6),
             child: Column(
-              children: [
-                CustomHomeTitle(),
-              ],
+              children: [],
             ),
           ),
         ),
