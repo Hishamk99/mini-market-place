115e8847aa590f6fc7072478b7e40f959dbb5f97
hishamkame <hishamkamel370@gmail.com>
2024-06-03 21:41:05 +0300

    design bottomNavigationBar (shape for add) part 3

--- lib/features/home/presentation/views/home_page.dart
+++ lib/features/home/presentation/views/home_page.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mini_nft_marketplace_app/constant.dart';
@@ -11,63 +10,16 @@ import 'package:mini_nft_marketplace_app/features/home/presentation/widgets/cust
 import 'package:mini_nft_marketplace_app/features/home/presentation/widgets/top_seller_category_list_view.dart';
 import 'package:mini_nft_marketplace_app/features/home/presentation/widgets/trending_category_list_view.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
   static String id = 'HomePage';
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int index = 0;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         extendBody: true,
-        bottomNavigationBar: ClipRRect(
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(50),
-            topRight: Radius.circular(50),
-          ),
-          child: BackdropFilter(
-            filter: ImageFilter.blur(
-              sigmaX: 10,
-              sigmaY: 10,
-            ),
-            child: Container(
-              color: Colors.white.withOpacity(.1),
-              height: 90,
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Icon(
-                    Icons.home,
-                    size: 39,
-                    color: Colors.white,
-                  ),
-                  Icon(
-                    FontAwesomeIcons.chartSimple,
-                    size: 39,
-                    color: Colors.white,
-                  ),
-                  SizedBox(width: 39),
-                  Icon(
-                    Icons.search,
-                    size: 39,
-                    color: Colors.white,
-                  ),
-                  Icon(
-                    Icons.person,
-                    size: 39,
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
+        bottomNavigationBar: const CustomBottomNavigationBar(),
         backgroundColor: kPrimaryColor,
         appBar: AppBar(
           title: Text(
@@ -91,11 +43,96 @@ class _HomePageState extends State<HomePage> {
                 CustomTopSellerText(),
                 SizedBox(height: 7),
                 TopSellerCategoryListViews(),
-                SizedBox(height: 100),
+                SizedBox(height: 110),
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  const CustomBottomNavigationBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 122.5,
+      child: Stack(
+        alignment: Alignment.center,
+        clipBehavior: Clip.none,
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(50),
+                topRight: Radius.circular(50),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 10,
+                  sigmaY: 10,
+                ),
+                child: Container(
+                  color: Colors.white.withOpacity(.1),
+                  height: 80,
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Icon(
+                        Icons.home,
+                        size: 39,
+                        color: Colors.white,
+                      ),
+                      Icon(
+                        FontAwesomeIcons.chartSimple,
+                        size: 39,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: 39),
+                      Icon(
+                        Icons.search,
+                        size: 39,
+                        color: Colors.white,
+                      ),
+                      Icon(
+                        Icons.person_2_outlined,
+                        size: 39,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 45,
+            child: Container(
+              width: 70,
+              height: 70,
+              decoration: const ShapeDecoration(
+                color: Color(0xffF7DFF0),
+                shape: StarBorder.polygon(
+                  pointRounding: .5,
+                  sides: 6,
+                ),
+              ),
+              child: const Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 35,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
