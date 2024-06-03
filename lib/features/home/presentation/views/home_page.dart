ed4148eb0cfbcbe7d6519c23762c97dd2c5b37b1
hishamkame <hishamkamel370@gmail.com>
2024-06-03 12:13:12 +0300

    create top seller list view widget

--- lib/features/home/presentation/views/home_page.dart
+++ lib/features/home/presentation/views/home_page.dart
@@ -1,4 +1,3 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:mini_nft_marketplace_app/constant.dart';
 import 'package:mini_nft_marketplace_app/core/resources/strings_manager.dart';
@@ -6,6 +5,7 @@ import 'package:mini_nft_marketplace_app/core/resources/styles.dart';
 import 'package:mini_nft_marketplace_app/features/home/presentation/widgets/category_image.dart';
 import 'package:mini_nft_marketplace_app/features/home/presentation/widgets/custom_trending_collection_text.dart';
 import 'package:mini_nft_marketplace_app/features/home/presentation/widgets/custom_top_seller_text.dart';
+import 'package:mini_nft_marketplace_app/features/home/presentation/widgets/top_seller_category_list_view.dart';
 import 'package:mini_nft_marketplace_app/features/home/presentation/widgets/trending_category_list_view.dart';
 
 class HomePage extends StatelessWidget {
@@ -38,7 +38,7 @@ class HomePage extends StatelessWidget {
                   SizedBox(height: 27),
                   CustomTopSellerText(),
                   SizedBox(height: 7),
-                  TopSellerCategoryItem(),
+                  TopSellerCategoryListViews(),
                   SizedBox(height: 10),
                 ],
               ),
@@ -50,100 +50,4 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class TopSellerCategoryItem extends StatelessWidget {
-  const TopSellerCategoryItem({
-    super.key,
-  });
 
-  @override
-  Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(27),
-      child: BackdropFilter(
-        filter: ImageFilter.blur(
-          sigmaX: 10,
-          sigmaY: 10,
-        ),
-        child: Container(
-          color: Colors.white.withOpacity(.1),
-          width: 157.6,
-          height: 236,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: Image.asset(
-                  'assets/images/topSeller1.png',
-                  width: 139.6,
-                  height: 139.6,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const SizedBox(width: 10),
-                  Text(
-                    'Abstract Pink',
-                    style: Styles.style12W600,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 6),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const SizedBox(width: 10),
-                  Text(
-                    'abstract #2384',
-                    style: Styles.style12W600.copyWith(
-                      color: Colors.grey,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 12),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Image.asset(
-                          'assets/images/path1599.png',
-                          width: 7.5,
-                          height: 15,
-                          color: Colors.grey,
-                        ),
-                        const SizedBox(width: 6),
-                        Text(
-                          '.98',
-                          style: Styles.style13,
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        const Icon(
-                          Icons.favorite,
-                          color: Colors.red,
-                          size: 16,
-                        ),
-                        const SizedBox(width: 5),
-                        Text(
-                          '200',
-                          style: Styles.style12,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/features/home/presentation/widgets/top_seller_category_list_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'top_seller_category_item.dart';
+
+class TopSellerCategoryListViews extends StatelessWidget {
+  const TopSellerCategoryListViews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 236,
+      child: ListView.builder(
+        itemCount: 3,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.only(right: 8.0),
+            child: TopSellerCategoryItem(),
+          );
+        },
+      ),
+    );
+  }
+}
