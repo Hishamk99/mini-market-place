2e2ab836cfaaab9123eeeb392bc8475fda64d2e6
hishamkame <hishamkamel370@gmail.com>
2024-06-04 14:35:12 +0300

    create state page and its route

---
+++ lib/features/stats/views/state_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StatePage extends StatelessWidget {
+  const StatePage({super.key});
+  static String id = 'StatePage';
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(),
+    );
+  }
+}
